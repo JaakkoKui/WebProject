4d8d2eef10856d52229d9e2527f140f2addb6096
Jetro <jetro.karjalainen@gmail.com>
2022-04-29 17:03:53 +0300

    main content otettu pois sidebar komponentit sisältä

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,17 +1,22 @@
 <template>
     <nav-bar></nav-bar>
     <side-bar></side-bar>
+    <div class="content">
+        <main-content></main-content>
+    </div>
 </template>
 
 <script>
 import navBar from "@/components/navBar";
 import sideBar from "@/components/sideBar";
+import mainContent from "@/components/mainContent";
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
+
 export default {
     name: "HomePage",
-    components: {navBar, sideBar}
+    components: {navBar, sideBar, mainContent}
 }
 </script>
 
