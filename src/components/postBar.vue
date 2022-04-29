e5f098191f3a3985ce7d30bfa1c22759b93a1a23
Jetro <jetro.karjalainen@gmail.com>
2022-04-29 17:30:01 +0300

    postin voi avata painamalla titleä (voisi ehkä olla nappi) post sivun tyylit eivät toimi vielä oikein

--- src/components/postBar.vue
+++ src/components/postBar.vue
@@ -3,7 +3,7 @@
     <div id="postBar">
         <button class="floatRight" aria-label='delete item' onClick='alert("You deleted the item!")' type='button'>X
         </button>
-        <h1>{{ post.title }}</h1>
+        <h1 @click="openPost">{{ post.title }}</h1>
         <p>{{ shortPost }}</p>
         <div class="floatRight">
             <button class="like">
@@ -24,6 +24,11 @@ export default {
     computed: {
         shortPost() {
             return this.post.content.substr(0, 50) + "..."
+        }
+    },
+    methods: {
+        openPost() {
+            this.$router.push("/post/" + this.post.id);
         }
     }
 }
